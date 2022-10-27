e210eec22557070aeb7cc49bf5815e2fac479b13
Sadid <sfhsadid@gmail.com>
2022-10-27 18:54:59 +0600

    foooter new update done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,32 +1,8 @@
-import { Link } from "react-router-dom";
 import "./Footer.css";
-
 const Footer = () => {
   return (
     <div>
-      <h1 className="mt-5 footer">
-        <div className="contLinkiner">
-          <div className="row">
-            <div className="footer-col">
-              <h4>compLinkny</h4>
-              <ul className="mainCala">
-                <li>
-                  <Link to="#">Linkbout us</Link>
-                </li>
-                <li>
-                  <Link to="#">our services</Link>
-                </li>
-                <li>
-                  <Link to="#">privLinkcy policy</Link>
-                </li>
-                <li>
-                  <Link to="#">LinkffiliLinkte progrLinkm</Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </h1>
+      <p className=" footer">ALL Rights Resived @ BY MERN Learning</p>
     </div>
   );
 };
